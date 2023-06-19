6e393d99ea0f5c799115176eac209fca224c1e8b
Henrique-Bidarte <henriqueb1208@gmail.com>
2023-06-19 19:42:25 -0300

    constant adjustment

--- frontend/src/constants/api.constant.js
+++ frontend/src/constants/api.constant.js
@@ -4,11 +4,12 @@ const API_METHOD = {
 };
 
 const API_ROUTES = {
+  HEALTHCHECK: "/healthcheck",
   NMAP_BASE_SCAN: "/scan",
   NMAP_PING: "/scan/ping",
   NMAP_AGRESSIVE: "/scan/agressive",
 };
 
-const API_URL = "http://localhost:3001/";
+const API_URL = "http://localhost:3001";
 
 export { API_METHOD, API_URL, API_ROUTES };
--- frontend/src/hooks/use-health-check.js
+++ frontend/src/hooks/use-health-check.js
@@ -1,10 +1,11 @@
+import { API_ROUTES } from "constants";
 import { useAxios } from "./use-axios";
 
 const useHealthCheck = () => {
   const { get } = useAxios();
 
   const getHealthCheck = async () => {
-    return await get("healthcheck");
+    return await get(API_ROUTES.HEALTHCHECK);
   };
 
   return {
